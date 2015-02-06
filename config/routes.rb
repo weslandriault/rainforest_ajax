5d1a430d1e6dd0c654427a98b354ac8d95a9d07c
Wes <wesley.landriault@gmail.com>
2015-02-06 17:49:34 -0500

    Created login functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  # get 'sessions/new'
 
-  get 'users/create'
+  # get 'sessions/create'
+
+  # get 'sessions/destroy'
+  resources :sessions, only: [:new, :create, :destroy]
+
+  # get 'users/new'
+
+  # get 'users/create'
   resources :users, only: [:new , :create]
 
   # get 'products/index'
