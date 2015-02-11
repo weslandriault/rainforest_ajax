d17e7caa838d34cd296e4ad291ce557fa308b2aa
Wes <wesley.landriault@gmail.com>
2015-02-10 19:00:16 -0500

    Created the reviews table, and added associations between all three of my tables.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+	has_many :reviews
+	has_many :users, through: :reviews
+
 	validates :name, presence: true
 	validates :description, presence: true
 	validates :price_in_cents, numericality: {only_integer: true}
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150210221534) do
+ActiveRecord::Schema.define(version: 20150210234750) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20150210221534) do
     t.integer  "price_in_cents"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "title"
+    t.text     "comment"
+    t.integer  "products_id"
+    t.integer  "users_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
